0e108493f567e12bd83e597cf623b5fdcbf9f223
Ridam jain <47075953+ridam258@users.noreply.github.com>
2019-06-03 17:42:26 +0530

    Create ridam3.py

---
+++ ridam/ridam3.py
@@ -0,0 +1,20 @@
+// troy pound to other weight units//
+
+tp = float(input("enter the weight in troy pounds: "))
+pound = tp*0.82286
+kg = tp*0.37324
+ounce = tp*13.166
+stone = tp*0.058776
+mt = tp*3.7324*10**-4
+gm = tp * 5760.0
+mgm = tp*373242
+microgm = tp*3.7324*10**8
+print("the converted weights are: ")
+print(str(poun)+ "pounds")
+print(str(kg)+ "kilograms")
+print(str(ounce)+ "ounces")
+print(str(stone)+ "stonnes")
+print(str(mt)+ "metric tonnes")
+print(str(gm)+ " grams")
+print(str(mgm)+ " milli grams")
+print(str(microgm)+ " micro grams")
