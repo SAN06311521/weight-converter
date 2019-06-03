8e9c067dcb8a3736db7fbb261e8f896737ad3d0e
Ridam jain <47075953+ridam258@users.noreply.github.com>
2019-06-03 17:13:34 +0530

    Create ridam1.py

---
+++ ridam/ridam1.py
@@ -0,0 +1,18 @@
+stone = float(input("enter the weight in stones: "))
+pounds = stone*14.0
+kg = stone*6.3503
+ounce = stone*224.0
+mt = stone*0.0063503
+tp = stone*17.014
+gm = stone*6350.3
+mgm = stone*6350293
+microgm = 6.35E+9
+print("the converted weights are: ")
+print(str(pounds)+"pounds")
+print(str(kg)+"kilograms")
+print(str(ounce)+"ounces")
+print(str(mt)+"metric tonnes")
+print(str(tp)+"troy pounds")
+print(str(gm)+"grams")
+print(str(mgm)+"milli grams")
+print(str(microgm)+"micro grams")
