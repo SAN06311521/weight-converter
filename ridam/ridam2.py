0e448c6f83540693933cb11a8b007f809cf83e56
Ridam jain <47075953+ridam258@users.noreply.github.com>
2019-06-03 17:22:53 +0530

    Create ridam2.py

---
+++ ridam/ridam2.py
@@ -0,0 +1,18 @@
+mt = float(input("enter the weight in metric tonnes: "))
+pound = mt*2204.62
+kg = mt*1000.0
+ounce= mt*35274.0
+stonne = mt*157.473
+tp = mt*2679.23
+gm = mt*1E+6
+mgm = mt*1E+9
+microgm = mt*1E+12
+print("the convereted weights are: ")
+print(str(pound)+"pounds")
+print(str(kg) + "kilograms")
+print(str(ounce)+ "ounces")
+print(str(stonne)+ "stonnes")
+print(str(tp)+ "troyp pounds")
+print(str(gm)+ "grams")
+print(str(mgm)+ "milli grams")
+print(str(microgm)+ "micro grams")
