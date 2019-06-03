527540ba7172902b2adf4100408e2687a08918f9
SAN06311521 <47052719+SAN06311521@users.noreply.github.com>
2019-06-03 13:29:05 +0530

    Create sanya2.py

---
+++ sanya2.py
@@ -0,0 +1,18 @@
+kg = float(input("enter the weight in kilograms: "))
+pound = kg*2.205
+ounce = kg*35.274
+stonne = kg*0.157
+mt = kg*0.001
+tp = kg*2.67923
+gm = kg*1000.0
+mgm = kg*1E+6
+microgm = kg*1E+9
+print("the converted weights are: ")
+print(str(pound)+ "pounds")
+print(str(ounce)+"ounces")
+print(str(stonne)+ "stones")
+print(str(mt)+"metric tonnes")
+print(str(tp)+"troy pounds")
+print(str(gm)+"grams")
+print(str(mgm)+"milli grams")
+print(str(microgm)+"micro grams")
